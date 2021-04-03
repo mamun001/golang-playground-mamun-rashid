b67169a6b446f176dc1ac69849245c7b0495a954
Mamun Rashid <mamun001@gmail.com>
2021-04-02 22:58:12 -0700

    first commit

---
+++ data_types.go
@@ -0,0 +1,18 @@
+// copied from https://gobyexample.com/values
+
+
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("go" + "lang")
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
