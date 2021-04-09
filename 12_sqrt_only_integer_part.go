15a872598db8ba4356253848b5e22776dc8fefea
Mamun Rashid <mamun001@gmail.com>
2021-04-08 21:21:08 -0700

    sqrt-int

---
+++ 12_sqrt_only_integer_part.go
@@ -0,0 +1,22 @@
+// output only the integer part of a number up to 2^31 -1
+// example input
+// 8
+// 99
+// 2000000000
+// output
+// 2
+// 9
+// 44721
+
+
+import (
+  "fmt"
+  "math"
+)
+
+func mySqrt(x int) int {
+    a := float64(x)
+    b := math.Sqrt(a)
+    //fmt.Println(b)
+    return int(b)
+}
