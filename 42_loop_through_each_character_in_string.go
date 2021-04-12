3bcb047692c7ed962464e76c2a0c1f61147efa69
Mamun Rashid <mamun001@gmail.com>
2021-04-12 11:34:44 -0700

    added loop through string

---
+++ 42_loop_through_each_character_in_string.go
@@ -0,0 +1,18 @@
+package main
+
+// Tested in golang playground
+
+import (
+	"fmt"
+)
+
+func main() {
+
+        // rune_value is basiclaly is character (turns out to be of type rune)
+        // since rune_value holds one "letter" at a time, you can now do what you want with each letter
+	for _, rune_value := range "mamunrashid" {
+		fmt.Println(string(rune_value))
+	}
+
+}
+
