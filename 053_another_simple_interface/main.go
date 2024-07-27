39c261b3e3c554f0b1c76f5026dc249085b6115d
Mamun Rashid <mamun001@gmail.com>
2024-07-27 09:35:30 -0700

    new version of car_rental interface

--- 053_another_simple_interface/main.go
+++ 053_another_simple_interface/main.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+// 4 parts
+// 1. define the interface type
+// 2. define a data structure type
+// 3. implement the methods (in this case just called rent)
+// 4. Use the interface in the main func
+
+type car_rental interface {
+	rent()
+}
+
+type car_model struct {
+	model string
+}
+
+func (cm car_model) rent() {
+	fmt.Println("Car Model Rented Is: ", cm.model)
+}
+
+func main() {
+	var mycar car_rental
+	mycar = car_model{"camry"}
+	mycar.rent()
+}
