f3347847d37e3d4f8773719ef6a60f8a0f3aeee7
Mamun Rashid <mamun001@gmail.com>
2024-08-03 06:24:46 -0700

    added bitshift

---
+++ 013_b_bitshift_example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%b \t %d \n", 1, 1)
+	fmt.Printf("%b \t %d \n", 1<<1, 1<<1)
+	fmt.Printf("%b \t %d \n", 1<<2, 1<<2)
+	fmt.Printf("%b \t %d \n", 1<<3, 1<<3)
+	fmt.Printf("%b \t %d \n", 1<<4, 1<<4)
+	fmt.Printf("%b \t %d \n", 1<<5, 1<<5)
+	fmt.Printf("%b \t %d \n", 1<<6, 1<<6)
+	fmt.Printf("%b \t %d \n", 1<<7, 1<<7)
+	fmt.Printf("%b \t %d \n", 1<<8, 1<<8)
+	fmt.Printf("%b \t %d \n", 1<<9, 1<<9)
+	fmt.Printf("%b \t %d \n", 1<<10, 1<<10)
+}
