0250ba95fa75421ed01fcbc4a5061598bfa59c3c
Mamun Rashid <mamun001@gmail.com>
2024-08-23 20:58:32 -0700

    init functions

---
+++ 102_init_functions/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 123: INIT FUNCSTION
+
+func init() {
+	fmt.Println("Hello from Init Func 1")
+}
+
+func init() {
+	fmt.Println("Hello from Init Func 2")
+}
+
+func main() {
+
+	fmt.Println("Hello from main Func")
+
+}
