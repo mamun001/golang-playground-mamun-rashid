0b588f9cfc9dc81feefc3aeb036079b55eba69b1
Mamun Rashid <mamun001@gmail.com>
2024-11-21 21:27:29 -0800

    default values of variables

---
+++ 005_variable_default_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var i int
+var f float32
+var b bool
+var s string
+
+func main() {
+	fmt.Println("Practicing Golang Again")
+	fmt.Println(i)
+	fmt.Println(f)
+	fmt.Println(b)
+	fmt.Println(s)
+
+}
