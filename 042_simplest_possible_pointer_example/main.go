bce149468f2865387639dbff1ec45bfd2485cff9
Mamun Rashid <mamun001@gmail.com>
2024-11-29 10:20:49 -0800

    042 pointer

---
+++ 042_simplest_possible_pointer_example/main.go
@@ -0,0 +1,11 @@
+
+package main
+
+import "fmt"
+
+func main() {
+   
+	var a int = 8
+	fmt.Println(a)   // prints 8
+        fmt.Println(&a)  // prints a memorry adderess. Think of & as "address of"
+}
