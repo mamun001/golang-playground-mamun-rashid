1908e31bce7354742fb30717df8217ec63340067
Mamun Rashid <mamun001@gmail.com>
2024-12-22 21:11:20 -0800

    leetcode 2769

---
+++ 900_ALL_leetcode_solutions/easy/leetcode_easy_2769_find_maximum_achievable_number/2769.go
@@ -0,0 +1,19 @@
+// 2769 Find the maximum achievable number
+
+// Acceptance rate = 90.7%
+
+// Had to get help for this one from youtube
+
+// the wording of the problem is so overworked!
+
+// basically x -t = num + t , solve for x , that's it!
+
+func theMaximumAchievableX(num int, t int) int {
+
+    // x -t = num + t
+    // x = num + 2 *t
+
+    return num + 2 * t
+    
+}
+
