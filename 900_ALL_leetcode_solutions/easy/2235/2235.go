d2477da92e17418ff54245aa4151c87a29e221e1
Mamun Rashid <mamun001@gmail.com>
2024-12-30 15:01:48 -0800

    2235 leetcode

---
+++ 900_ALL_leetcode_solutions/easy/2235/2235.go
@@ -0,0 +1,13 @@
+// WHY IS THIS EVEN A LEETCODE Problem???
+
+// easy
+
+// 2235, add two integers
+
+// acc rate 88.1 pct
+
+// DEC 30, 204
+
+func sum(num1 int, num2 int) int {
+   return num1 + num2
+}
