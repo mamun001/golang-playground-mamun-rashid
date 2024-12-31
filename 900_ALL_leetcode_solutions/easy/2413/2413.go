5407415929f13508156d4c8c2b13a7d04aa350f3
Mamun Rashid <mamun001@gmail.com>
2024-12-30 23:44:09 -0800

    2413 leetcode

---
+++ 900_ALL_leetcode_solutions/easy/2413/2413.go
@@ -0,0 +1,17 @@
+// easy 
+// 2413 smallest even multiple
+
+// 88 pct acc rate
+
+// How is this even a leetcode problem???
+
+func smallestEvenMultiple(n int) int {
+
+    if n % 2 == 0 {
+        return n
+    } else {
+        return n * 2
+    }
+    
+}
+
